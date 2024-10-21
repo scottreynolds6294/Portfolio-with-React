08c5d74624762df45ba61fa60e3f219517f1f6f0
scottreynolds6294@gmail.com <scottreynolds6294@gmail.com>
2024-10-21 13:52:51 -0400

    main.jsx started and index.html set up

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import 
+
+const router = createBrowserRouter([
+    {
+
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+);
