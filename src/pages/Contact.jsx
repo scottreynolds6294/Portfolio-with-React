be675b9687f29ef3676ac53a8e67d3bbcc8fe93a
scottreynolds6294@gmail.com <scottreynolds6294@gmail.com>
2024-10-21 14:44:22 -0400

    contact form implemented

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+
+export default function Contact() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const [errors, setErrors] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+
+        setErrors  ((prevErrors) => ({
+            ...prevErrors,
+            [name]: "",
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let valid = true;
+        const newErrors = {};
+
+        if (!formData.name) {
+            newErrors.name = "Name is required";
+            valid = false;
+        }
+
+        if (!formData.email) {
+            newErrors.email = "Email is required";
+            valid = false;
+        } else if (!emailRegex.test(formData.email)) {
+            newErrors.email = "Please enter a valid email address";
+            valid = false;
+        }
+
+        if (!formData.message) {
+            newErrors.message = "Message is required";
+            valid = false;
+        }
+
+        if (valid) {
+            console.log('Form submitted:', formData);
+            alert("Form submitted successfully");
+            setFormData({
+                name: "",
+                email: "",
+                message: "",
+            });
+        } else {
+            setErrors(newErrors);
+        }
+    };
+
+    const handleBlur = (e) => {
+        const { name } = e.target;
+
+        if (!formData[name]) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                [name]: `${name.charAt(0).toUpperCase() + name.slice(1)} is required`,
+            }));
+        } else if (name === "email" && !emailRegex.test(formData.email)) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                email: "Please enter a valid email address",
+            }));
+        }
+    };
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <form onSubmit={handleSubmit} noValidate>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    />
+                    {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                    type= "email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    />
+                    {errors.email && <p style={{ color: "red"}}>{errors.email}</p>}
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    />
+                    {errors.message && <p style={{ color: "red"}}>{errors.message}</p>}
+                </div>
+
+                <button type="submit">Submit</button>
+
+            </form>
+        </div>
+    );
+}
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,55 @@
+export default function Portfolio() {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <section class="my-work mb-5" id="work">
+        <div class="my-work-row">
+            <div class="portfolio-content">
+                <div class="images-grid d-grid gap-4 ">
+                    {/* <div class="grid-item">
+                        <a href="https://trip-planner-b5zi.onrender.com/">
+                            <img src="./assets/images/Screenshot 2024-09-27 at 4.02.44 PM.jpg" alt="Trip Planner App Image"></img>
+                            <div class="image-hover-text">Deployed on Render. Applications deployed on Render can take longer to load.</div>
+                        </a>
+                        <div class="title">Trip Planner App</div>
+                    </div> */}
+                    <div class="grid-item">
+                        <a href="https://tech-blog-ikrx.onrender.com/">
+                            <img src="./assets/images/Screenshot 2024-09-27 at 4.03.08 PM.jpg" alt="Tech Blog Image"></img>
+                            <div class="image-hover-text">Deployed on Render. Applications deployed on Render can take longer to load.</div>
+                        </a>
+                        <div class="title">Tech Blog Demo</div>
+                    </div>
+                    <div class="grid-item">
+                        <a href="https://scottreynolds6294.github.io/Weather-Dashboard/">
+                            <img src="./assets/images/Screenshot 2024-09-27 at 4.28.36 PM.jpg" alt="Weather DashboardImage"></img>
+                        </a>
+                        <div class="title">Weather Dashboard</div>
+                     </div>
+                     <div class="grid-item">
+                        <a href="https://natale565.github.io/movie-night/">
+                            <img src="./assets/images/Screenshot 2024-09-27 at 4.46.07 PM.jpg" alt="Movie Database Image"></img>
+                        </a>
+                        <div class="title">Movie Database App</div>
+                     </div>
+                     <div class="grid-item">
+                        <a href="https://note-taker-app-in84.onrender.com/">
+                            <img src="./assets/images/Screenshot 2024-09-27 at 6.23.47 PM.jpg" alt="Note Taking App Image"></img>
+                            <div class="image-hover-text">Deployed on Render. Applications deployed on Render can take longer to load.</div>
+                        </a>
+                        <div class="title">Note Taking App</div>
+                     </div>
+                     <div class="grid-item">
+                     <a href="https://app.screencastify.com/v3/watch/qKKIHDRTjmB1yXZDvGhE">
+                        <img src="./assets/images/Screenshot 2024-09-27 at 6.28.34 PM.jpg" alt="E-Commerce Backend Image"></img>
+                        <div class="image-hover-text">Watch the video, demonstrating the functionality of an e-commerce back end.</div>
+                    </a>
+                    <div class="title">E-Commerce Back End</div>
+                 </div>
+            </div>
+        </div>
+     </div>
+    </section>
+</div>
+    );
+}
